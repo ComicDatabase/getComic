7719788647998becaf9fb662ef871d95d5b8e85e
feilong <614183595@qq.com>
2017-04-16 20:58:23 +0800

    fix

--- main.php
+++ main.php
@@ -1,7 +1,14 @@
 <?php
+    if (!file_exists("cm.txt"))
+    {
+        $myfile = fopen("cm.txt", "w") or die("error1!");
+        fwrite($myfile,"\r\n");
+        fclose($myfile);
+    }
+
     if(isset($_POST['comic']))
     {
-        $myfile = fopen("cm.txt", "a+") or die("error!");
+        $myfile = fopen("cm.txt", "a+") or die("error2!");
         fwrite($myfile,base64_decode($_POST['comic']));
         fwrite($myfile,"\r\n");
         fclose($myfile);
@@ -11,7 +18,7 @@
     {
         if($_GET['clear'] == 1)
         {
-            $myfile = fopen("cm.txt", "w") or die("error!");
+            $myfile = fopen("cm.txt", "w") or die("error3!");
             fwrite($myfile,"\n");
             fclose($myfile);
             echo "ComicClear!";
@@ -19,7 +26,7 @@
     }
     else {
         echo "list:";
-        $myfile = fopen("cm.txt", "r") or die("error!");
+        $myfile = fopen("cm.txt", "r") or die("error4!");
         echo fread($myfile,filesize("cm.txt"));
         fclose($myfile);
     }
