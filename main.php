0644bbbe9e16cf9d99dfa6174c911c84891cea4f
feilong <614183595@qq.com>
2017-04-16 20:26:16 +0800

    version 1

--- main.php
+++ main.php
@@ -1,10 +1,26 @@
-<html>
-    <head>
-        <title>test</title>
-    </head>
-    <body>
-        <?php
-        echo "test";
-         ?>
-     </body>
-</html>
+<?php
+    if(isset($_POST['comic']))
+    {
+        $myfile = fopen("cm.txt", "a+") or die("error!");
+        fwrite($myfile,base64_decode($_POST['comic']));
+        fwrite($myfile,"\r\n");
+        fclose($myfile);
+        echo "ComicSave!";
+    }
+    else if(isset($_GET['clear']))
+    {
+        if($_GET['clear'] == 1)
+        {
+            $myfile = fopen("cm.txt", "w") or die("error!");
+            fwrite($myfile,"\n");
+            fclose($myfile);
+            echo "ComicClear!";
+        }
+    }
+    else {
+        echo "list:";
+        $myfile = fopen("cm.txt", "r") or die("error!");
+        echo fread($myfile,filesize("cm.txt"));
+        fclose($myfile);
+    }
+ ?>
