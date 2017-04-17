c66f1a191b0568ccc1e9f2cee54d683df9c039e2
feilong <614183595@qq.com>
2017-04-18 07:27:19 +0800

    gae存储貌似只有正式部署之后才能使用

--- main.php
+++ main.php
@@ -1,4 +1,33 @@
 <?php
+
+/**
+ * Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+# [START use_cloud_storage_tools]
+    use google\appengine\api\cloud_storage\CloudStorageTools;
+# [END use_cloud_storage_tools]
+
+    $newFileContent = "test";
+    $my_bucket = "txcomic-164810.appspot.com";
+    # [START write_simple]
+    file_put_contents("gs://${my_bucket}/hello.txt", $newFileContent);
+
+
+
+
+	/*
     if (!file_exists($file))
     {
         $myfile = fopen($file, "w") or die("error1!");
@@ -30,4 +59,5 @@
         echo fread($myfile,filesize($file));
         fclose($myfile);
     }
+	*/
  ?>
